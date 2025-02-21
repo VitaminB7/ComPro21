65f96b49a9f58250f6f1bae4706e95f75c2e9411
ZiV99 <71887602+VitaminB7@users.noreply.github.com>
2025-02-21 15:18:16 +0700

    average.cpp Completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+using namespace std;
 
+int main(int argc,char* argv[])
+{
+    if(argc >= 2) {
+        double sum=0;
+        int N = argc-1;
+        double av;
+        for(int i = 1; i < argc ;i++)
+        {
+            sum += atof(argv[i]);
+        }
+        av = sum/N;
+        cout<<"---------------------------------\n";
+        cout<<"Average of "<<N<<" numbers = "<< av; 
+        cout<<"\n---------------------------------";
+    }
+    else {cout<<"Please input numbers to find average.";}
+}
